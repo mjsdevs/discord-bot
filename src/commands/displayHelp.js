0e1d201ab145ae071e059cb5becb6b018966455f
Romildo Júnior <jrmmendes@outlook.com>
2020-03-29 20:38:33 -0300

    Fix typo

--- src/commands/displayHelp.js
+++ src/commands/displayHelp.js
@@ -2,7 +2,7 @@ const { COMMAND_PREFIX: prefix } = process.env;
 
 module.exports = (message) => {
   message.reply('Hello!');
-  message.channel.send('Here\'s a list of avaliable commands:');
+  message.channel.send('Here\'s a list of available commands:');
   message.channel.send(`- \`${prefix}help\`: Show this help.`);
   message.channel.send(`- \`${prefix}save {briefing} [tag1, tag2] <https://myLink.com>\`: Saves something to the library.`);
 };
